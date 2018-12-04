fd7721c5bba981adac912f22f5cc25f4fb1dd83a
Christian Görtz <44924538+chrisgoertz@users.noreply.github.com>
2018-12-04 21:23:54 +0100

    Arduino sketch added, minor code change in rfidtable

--- src/main/java/de/ncdf/dbprepare/RfidTableController.java
+++ src/main/java/de/ncdf/dbprepare/RfidTableController.java
@@ -2,13 +2,16 @@ package de.ncdf.dbprepare;
 
 
 import java.net.URL;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 import de.ncdf.dbconnections.StampDB;
 import de.ncdf.models.RFIDTag;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class RfidTableController {
 
@@ -22,7 +25,7 @@ public class RfidTableController {
     private TableView<RFIDTag> rfidTable;
 
     @FXML
-    private TableColumn<RFIDTag, String> columnRDID;
+    private TableColumn<RFIDTag, String> columnRFID;
 
     @FXML
     private TableColumn<RFIDTag, String> columnStatus;
@@ -33,13 +36,25 @@ public class RfidTableController {
     @FXML
     void initialize() {
         assert rfidTable != null : "fx:id=\"rfidTable\" was not injected: check your FXML file 'RfidTable.fxml'.";
-        assert columnRDID != null : "fx:id=\"columnRDID\" was not injected: check your FXML file 'RfidTable.fxml'.";
+        assert columnRFID != null : "fx:id=\"columnRDID\" was not injected: check your FXML file 'RfidTable.fxml'.";
         assert columnStatus != null : "fx:id=\"columnStatus\" was not injected: check your FXML file 'RfidTable.fxml'.";
         assert columnZeitpunkt != null : "fx:id=\"columnZeitpunkt\" was not injected: check your FXML file 'RfidTable.fxml'.";
-
+        initTable();
+    }
+    
+    private void initTable() {
+    	columnRFID.setCellValueFactory(new PropertyValueFactory<>("tagString"));
+    	columnRFID.setMinWidth(100.0);
+    	columnStatus.setCellValueFactory(new PropertyValueFactory<>("statusString"));
+    	columnStatus.setMinWidth(40.0);
+    	columnZeitpunkt.setCellValueFactory(new PropertyValueFactory<>("timeStampString"));
+    	columnZeitpunkt.setMinWidth(150.0);
+    	getStamps();
     }
     private void getStamps() {
     	StampDB sd = new StampDB();
+    	ObservableList<RFIDTag> tagList = sd.getAll();
+    	rfidTable.setItems(tagList);
     	
     }
 }
