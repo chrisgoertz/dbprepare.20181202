39c7abcd21b8d1121ddc8eb4b73c611be416b3f4
chrisgoertz <chris.goertz@gmx.de>
2018-12-27 14:26:31 +0100

    dbobject for getting today's timestamps

---
+++ src/main/java/de/ncdf/dbconnections/AnwesenheitDB.java
@@ -0,0 +1,6 @@
+package de.ncdf.dbconnections;
+
+public class AnwesenheitDB {
+	//SQL for getting today's timestamps
+	//SELECT * FROM stampevents WHERE DATE(`timestmp`) = CURDATE()
+}
--- src/main/java/de/ncdf/models/Anwesenheit.java
+++ src/main/java/de/ncdf/models/Anwesenheit.java
@@ -1,5 +1,59 @@
 package de.ncdf.models;
 
-public class Anwesenheit {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
+public class Anwesenheit {
+	StringProperty nachname = new SimpleStringProperty();
+	StringProperty vorname = new SimpleStringProperty();
+	StringProperty kommt = new SimpleStringProperty();
+	StringProperty geht = new SimpleStringProperty();
+	StringProperty konto = new SimpleStringProperty();
+	
+	public void setNachname(String s) {
+		this.nachname.set(s);
+		
+	}
+	public String getNachname() {
+		return this.nachname.get();
+	}
+	public StringProperty Nachname() {
+		return this.nachname;
+	}
+	public void setVorname(String s) {
+		this.vorname.set(s);
+	}
+	public String getVorname() {
+		return this.vorname.get();
+	}
+	public StringProperty Vorname() {
+		return this.vorname;
+	}
+	public void setKommt(String s) {
+		this.kommt.set(s);
+	}
+	public String getKommt() {
+		return this.kommt.get();
+	}
+	public StringProperty Kommt() {
+		return this.kommt;
+	}
+	public void setGeht(String s) {
+		this.geht.set(s);
+	}
+	public String getGeht() {
+		return this.geht.get();
+	}
+	public StringProperty Geht() {
+		return this.geht;
+	}
+	public void setKonto(String s) {
+		this.konto.set(s);
+	}
+	public String getKonto() {
+		return this.konto.get();
+	}
+	public StringProperty Konto() {
+		return this.konto;
+	}
 }
